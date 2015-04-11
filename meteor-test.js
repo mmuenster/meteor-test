c59da2b5df45ac1e54a3c53c4c6ec327394ab17c
Matthew Muenster <matt.muenster@gmail.com>
2015-04-11 14:17:55 -0500

    First commit

---
+++ meteor-test.js
@@ -0,0 +1,91 @@
+Handlebars.registerHelper('isAdminUser', function() {
+  return Roles.userIsInRole(Meteor.user(), ['admin']);
+});
+
+Accounts.config({
+    forbidClientAccountCreation: true
+})
+
+accountsAdminUiConfiguration = {
+  userStatus: true, //if true and user-status pacakge installed, this will show last login and current login status
+};
+
+if (Meteor.isClient) {
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+  });
+
+  
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // bootstrap the admin user if they exist -- You'll be replacing the id later
+        if (Meteor.users.findOne("your_admin_user_id"))
+            Roles.addUsersToRoles("your_admin_user_id", ['admin']);
+
+        // create a couple of roles if they don't already exist (THESE ARE NOT NEEDED -- just for the demo)
+        if(!Meteor.roles.findOne({name: "secret"}))
+            Roles.createRole("secret");
+
+        if(!Meteor.roles.findOne({name: "double-secret"}))
+            Roles.createRole("double-secret");
+    });
+}
+
+if (Meteor.isClient) {
+    Template.adminTemplate.helpers({
+        // check if user is an admin
+        isAdminUser: function() {
+            return Roles.userIsInRole(Meteor.user(), ['admin']);
+        }
+    })
+}
+
+Router.configure({
+    layoutTemplate: 'layout'
+});
+
+Router.map(function() {
+    this.route('home', {
+        path: '/',
+        template: 'home'
+    });
+
+    this.route('admin', {
+        path:'/admin',
+        template: 'accountsAdmin',
+        onBeforeAction: function() {
+            if (Meteor.loggingIn()) {
+                this.render(this.loadingTemplate);
+            } else if(!Roles.userIsInRole(Meteor.user(), ['admin'])) {
+                console.log('redirecting');
+                this.redirect('/');
+            } else {
+              this.next();
+            }
+        }
+    });
+});
---
+++ run.bat
@@ -0,0 +1,2 @@
+set MONGO_URL=mongodb://mmuenster:mmuenster@ds061371.mongolab.com:61371/newlis
+meteor

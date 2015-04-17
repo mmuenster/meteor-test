bb0f06c163a136e73197a2b3e68668baf3d8444a
Matthew Muenster <matt.muenster@gmail.com>
2015-04-17 14:45:04 -0500

    More changes

--- meteor-test.js
+++ meteor-test.js
@@ -1,3 +1,39 @@
+    requireAdmin = function () {
+      if(!Roles.userIsInRole(Meteor.user(), ['admin'])) {
+              this.redirect('/');
+            } else {
+              this.next();
+            }
+    }
+
+    Router.configure({
+        layoutTemplate: 'layout',
+        loadingTemplate: 'chat',
+        notFoundTemplate: 'caseList'
+    });
+
+    Router.onBeforeAction(function() {
+      if (! Meteor.userId()) {
+        this.render('login');
+      } else {
+        this.next();
+      }
+    });
+
+    Router.route('/', function() {
+      this.redirect('/caseList');
+    });
+
+    Router.route('caseList');
+
+    Router.route('chat');
+
+    Router.route('admin', {
+        path:'/admin',
+        template: 'accountsAdmin',
+        onBeforeAction: requireAdmin
+    });
+
 Handlebars.registerHelper('isAdminUser', function() {
   return Roles.userIsInRole(Meteor.user(), ['admin']);
 });
@@ -6,37 +42,19 @@ Accounts.config({
     forbidClientAccountCreation: false
 })
 
-if (Meteor.isClient) {
+Messages = new Mongo.Collection("messages")
+Cases = new Mongo.Collection("cases")
 
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
 
 if (Meteor.isServer) {
+    Meteor.publish('allCases', function(filter) {
+      return Cases.find();
+    })
 
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
-
-if (Meteor.isServer) {
     Meteor.startup(function () {
         // bootstrap the admin user if they exist -- You'll be replacing the id later
-        if (Meteor.users.findOne("your_admin_user_id"))
-            Roles.addUsersToRoles("your_admin_user_id", ['admin']);
+        if (Meteor.users.findOne("uLLxN3XTTTtDHM8Bk"))
+            Roles.addUsersToRoles("uLLxN3XTTTtDHM8Bk", ['admin']);
 
         // create a couple of roles if they don't already exist (THESE ARE NOT NEEDED -- just for the demo)
         if(!Meteor.roles.findOne({name: "secret"}))
@@ -48,36 +66,64 @@ if (Meteor.isServer) {
 }
 
 if (Meteor.isClient) {
-    Template.adminTemplate.helpers({
-        // check if user is an admin
-        isAdminUser: function() {
-            return Roles.userIsInRole(Meteor.user(), ['admin']);
+
+  Meteor.subscribe('allCases');
+  console.log("under meteor subscribe")
+
+  Template.chat.helpers({
+    messages: function() {
+      return Messages.find();
+    }
+  })
+
+  Template.chat.events({
+    "submit #new-message": function (event) {
+      var text = event.target.text.value;
+
+      Messages.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
+
+  var myApp = angular.module('myApp', ['angular-meteor']);
+
+  myApp.config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+  });
+
+  myApp.controller('CaseController', ['$scope', '$meteor', function($scope, $meteor) {
+    console.log("In the controller")
+    $scope.cases = Cases.find().fetch(); //$meteor.collection(Cases);
+
+    $scope.orderBy = "receivedDate";
+    $scope.reverse = false;
+    $scope.orderString = "";
+    
+    $scope.changeSortOrder = function(sortKey) {
+      if(sortKey==$scope.orderBy) {
+        $scope.reverse = !$scope.reverse
+        if ($scope.reverse) {
+          $scope.orderString = "-" + $scope.orderBy;
+        } else {
+          $scope.orderString = $scope.orderBy;
         }
-    })
+      } else {
+        $scope.orderBy = sortKey;
+        $scope.reverse = false;
+        $scope.orderString = $scope.orderBy;
+      }
+    }
+
+  }])
 }
 
-Router.configure({
-    layoutTemplate: 'layout'
-});
 
-Router.map(function() {
-    this.route('home', {
-        path: '/',
-        template: 'home'
-    });
-
-    this.route('admin', {
-        path:'/admin',
-        template: 'accountsAdmin',
-        onBeforeAction: function() {
-            if (Meteor.loggingIn()) {
-                this.render(this.loadingTemplate);
-            } else if(!Roles.userIsInRole(Meteor.user(), ['admin'])) {
-                console.log('redirecting');
-                this.redirect('/');
-            } else {
-              this.next();
-            }
-        }
-    });
-});

81c2cd45a0df63fe78ae5c5c4d8aaab14054881b
Matthew Muenster <matt.muenster@gmail.com>
2015-04-21 12:10:00 -0500

    More pdfBuilder progress

--- client/pdfBuilder.js
+++ client/pdfBuilder.js
@@ -1,24 +1,52 @@
+var averoLogoImg = "";
+convertImgToBase64("http://localhost:3000/images/AveroLogo.png", function(base64Img) {
+	averoLogoImg = base64Img;
+});
+
 Template.pdfBuilder.rendered = function() {
 	var ace = AceEditor.instance("editor",{
 	    theme:"monokai", 
 	    mode:"javascript"
 	});
 	ace.$blockScrolling = Infinity;
-	ace.setValue("This is some text")
+	//ace.setValue("This is some text"
+
+}
+
+function convertImgToBase64(url, callback, outputFormat){
+	var canvas = document.createElement('CANVAS');
+	var ctx = canvas.getContext('2d');
+	var img = new Image;
+	img.crossOrigin = 'Anonymous';
+	img.onload = function(){
+		canvas.height = img.height;
+		canvas.width = img.width;
+	  	ctx.drawImage(img,0,0);
+	  	var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+	  	callback.call(this, dataURL);
+        // Clean up
+	  	canvas = null; 
+	};
+	img.src = url;
 }
 
 Template.pdfBuilder.events({
-	"click #test": function(event) {
+	"click #test": function(event, template) {
+		console.log(averoLogoImg)
 		AceEditor.instance("editor", null, function(editor){
-			Meteor.call("evaluateCode", editor.getValue(), 
-				function (error, result) {
-				  if (error) {
-				    console.log(error)
-				  } else {
-				    console.log(result)
-				  }
-				}
-			)
+			var doc = new jsPDF("portrait", "pt", "letter");
+			eval(editor.getValue())
+			var string = doc.output('datauristring');
+			template.find("#previewPanel").src =  string;
+			// Meteor.call("evaluateCode", editor.getValue(), 
+			// 	function (error, result) {
+			// 	  if (error) {
+			// 	    console.log(error)
+			// 	  } else {
+			// 	    console.log(result)
+			// 	  }
+			// 	}
+			// )
 		});
 	},
 
